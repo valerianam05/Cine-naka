c7a0e935109c45136f700107663aef2a5de02259
valeria <hei.valeria.2@gmail.com>
2026-08-02 22:21:36 +0300

    feat(database): add user and reservation JPA entities

---
+++ src/main/java/com/example/demo/entity/UserEntity.java
@@ -0,0 +1,46 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.util.UUID;
+
+import com.example.demo.entity.enums.UserRole;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@AllArgsConstructor
+public class UserEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @Column(nullable = false)
+  private String firstName;
+
+  @Column(nullable = false)
+  private String lastName;
+
+  private LocalDate birthdate;
+
+  @Column(nullable = false, unique = true)
+  private String email;
+
+  @Column(nullable = false)
+  private String password;
+
+  private String phone;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private UserRole role;
+
+  protected UserEntity() {}
+
+}
