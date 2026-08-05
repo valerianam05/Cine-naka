912a69d8fe61e436f4ebc5e8fa5332679ddd12f8
deep-awak <mahery.razafimananatsoa.dev@gmail.com>
2026-08-05 00:06:00 -1100

    fix: uer entity

--- src/main/java/com/example/demo/entity/UserEntity.java
+++ src/main/java/com/example/demo/entity/UserEntity.java
@@ -10,7 +10,8 @@ import lombok.Data;
 
 @Entity
 @Table(name = "users")
-@Data
+@Getter
+@Setter
 @Builder
 @AllArgsConstructor
 public class UserEntity {
@@ -40,21 +41,4 @@ public class UserEntity {
   private UserRole role;
 
   protected UserEntity() {}
-
-  public UserEntity(
-      String firstName,
-      String lastName,
-      LocalDate birthdate,
-      String email,
-      String password,
-      String phone,
-      UserRole role) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.birthdate = birthdate;
-    this.email = email;
-    this.password = password;
-    this.phone = phone;
-    this.role = role;
-  }
 }
